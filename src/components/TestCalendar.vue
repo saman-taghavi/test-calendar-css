fa1171ec3ee2a1726760d8b43837f6048942b4a6
Saman Taghavi <samantaghavisani@gmail.com>
2022-02-11 21:26:07 +0330

    Add test calendar component

---
+++ src/components/TestCalendar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row align-content="center" justify="center">
+    <v-col cols="3">
+      <v-text-field
+        v-model="date"
+        name="name"
+        label="label"
+        id="id"
+      ></v-text-field>
+      <date-picker v-model="date"></date-picker>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "TestCalendar",
+  data() {
+    return {
+      date: "",
+    };
+  },
+};
+</script>
+
+<style></style>
