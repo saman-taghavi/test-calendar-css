5e6c8265c1972b3f9abd1f3cba62ad0d59dee0a7
Saman Taghavi <samantaghavisani@gmail.com>
2022-02-11 21:25:37 +0330

    Add cue-persian-datepicker

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VuePersianDatetimePicker from "vue-persian-datetime-picker";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component("date-picker", VuePersianDatetimePicker);
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
